0e9ae5221ae247a726550cc357a95d744b36cab6
Atelier Renard <atelierrenardparis@gmail.com>
2018-01-10 10:40:54 +0100

    update

---
+++ download.py
@@ -0,0 +1,11 @@
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+page = requests.get('https://www.azlyrics.com/m/megadeth.html', headers=headers)
+
+# print(page.content)
+
+f = open('megadeth_songs.html', 'w', encoding='utf-8')
+f.write(page.text)
+f.close()
