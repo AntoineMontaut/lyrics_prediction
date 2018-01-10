49d6b242bb22dd9a51cf8ce32b83fc423c85ed5a
Atelier Renard <atelierrenardparis@gmail.com>
2018-01-10 13:00:38 +0100

    update

--- parse_song_links.py
+++ parse_song_links.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import time
+import os
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -30,7 +31,7 @@ url_artist = 'megadeth/'
 
 # link = links[0]
 
-for link in links[:5]:
+for link in links:
 	file_name = link.split('/')[-1]
 	# song_name = file_name.replace('.html', '')
 	full_url = url_base + url_artist + file_name
@@ -59,9 +60,13 @@ for link in links[:5]:
 	lyrics = lyrics.replace('<br>', '\n')
 
 	# write the lyrics in a text file
-	f = open('lyrics/' + song_name + '.txt', 'w', encoding='utf-8')
-	f.write(lyrics)
-	f.close()
+	path = 'lyrics/' + song_name + '.txt'
+	if os.path.exists(path):
+		print('{} has already been downloaded'.format(song_name))
+	else:
+		f = open(path, 'w', encoding='utf-8')
+		f.write(lyrics)
+		f.close()
 
-	print('{} imported'.format(song_name))
-	time.sleep(90)
+		print('{} imported'.format(song_name))
+		time.sleep(90)
